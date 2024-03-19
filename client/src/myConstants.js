bd5586eff9f8390036a7c212ca58b3e25bbf65e9
VenisPatel <113828439+venispatel453@users.noreply.github.com>
2024-03-19 10:54:54 +0530

    Update myConstants.js

--- client/src/myConstants.js
+++ client/src/myConstants.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://ec2-13-200-229-108.ap-south-1.compute.amazonaws.com:8000";
+export const BACKEND_URL = "http://ec2-3-110-223-48.ap-south-1.compute.amazonaws.com:8000";
